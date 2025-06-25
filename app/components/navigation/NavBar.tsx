b1e222d8ad8b9088cc75ed6846ac52d821de5752
Joshua Gretton <jb.gretton@googlemail.com>
2025-06-25 13:06:04 +0100

    Added navbar

---
+++ app/components/navigation/NavBar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+	return (
+		<header className="w-screen h-auto border-b-2 border-gray-200">
+			<div className="max-w-7xl mx-auto p-5 flex flex-row items-center gap-10">
+				<h1 className="font-semibold text-lg">
+					<Link className="hover:underline" href="/">
+						Stats
+					</Link>
+				</h1>
+				<div className="flex-1">
+					<ul>
+						<li>
+							<Link className="hover:underline" href="/teams">
+								Teams
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "./components/navigation/NavBar";
 export const metadata: Metadata = {
 	title: "Volleyball Stats",
 	description: "Volleybell stats app.",
@@ -12,7 +13,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`antialiased`}>{children}</body>
+			<body className={`antialiased`}>
+				<div className="">
+					<NavBar />
+				</div>
+				<main className="mt-10">{children}</main>
+			</body>
 		</html>
 	);
 }
