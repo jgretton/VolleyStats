eff08b16292f4efb1e41d0a326df4146f96bf16f
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-03 18:51:05 +0100

    Redesign MatchInfoCard
    Refactored the MatchInfoCard component to center the venue badge and display team names and match date in a more visually balanced layout. The new design separates the team names and opponent with a clear 'VS' indicator and centers the date, enhancing readability and overall presentation.

--- app/components/cards/MatchInfoCard.tsx
+++ app/components/cards/MatchInfoCard.tsx
@@ -9,18 +9,37 @@ const MatchInfoCard = ({ matchData }: { matchData: Match }) => {
 	const { getTeamById } = useClubStore();
 	const { name: teamName } = getTeamById(matchData.teamId);
 	return (
-		<div className="bg-white border border-gray-200 rounded-lg p-4 mb-6">
-			<div className="flex items-center gap-4">
-				<h3 className="text-lg font-medium text-gray-900">
-					{teamName} vs {matchData.opponent}
-				</h3>
-				<span className="text-gray-400">•</span>
-				<span className="text-sm text-gray-600">
-					<FormatDate date={matchData.date} />
-				</span>
+		<div className="bg-white border border-gray-200 rounded-lg p-4">
+			<div className="text-center">
 				<VenueBadge venue={matchData.venue} />
 			</div>
+			<div className="flex gap-10 justify-center items-start mt-4">
+				<span className="text-lg font-medium text-gray-900 flex-1 text-end">
+					{teamName}
+				</span>
+				<div className="flex flex-col text-center gap-2">
+					<span className="text-gray-800 font-medium text-lg">VS</span>
+					<span className="text-sm text-gray-500">
+						<FormatDate date={matchData.date} />
+					</span>
+				</div>
+				<span className="text-lg font-medium text-gray-900 flex-1 text-start">
+					{matchData.opponent}
+				</span>
+			</div>
 		</div>
+		// <div className="bg-white border border-gray-200 rounded-lg p-4">
+		// 	<div className="flex items-center gap-4">
+		// 		<h3 className="text-lg font-medium text-gray-900">
+		// 			{teamName} vs {matchData.opponent}
+		// 		</h3>
+		// 		<span className="text-gray-400">•</span>
+		// 		<span className="text-sm text-gray-600">
+		// 			<FormatDate date={matchData.date} />
+		// 		</span>
+		// 		<VenueBadge venue={matchData.venue} />
+		// 	</div>
+		// </div>
 	);
 };
 
