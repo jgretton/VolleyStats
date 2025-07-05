b992061d0debe060bb2128a9c3b7335289c2abbc
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-05 10:33:52 +0100

    Create StartingLineupSelection.tsx

---
+++ app/components/matches/StartingLineupSelection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+/*
+Options for design
+- half volleyball court with buttons on positions, user selects starting players
+- another list of squad players and move over to starting lineup.
+
+
+Things to think about
+- the list of non selected players will be under the heading of bench.
+- 
+
+*/
+
+const StartingLineupSelection = () => {
+	return (
+		<div>
+			<div className="h-96 w-80 border rounded-lg border-gray-100 bg-gray-50 mt-10"></div>
+		</div>
+	);
+};
+
+export default StartingLineupSelection;
